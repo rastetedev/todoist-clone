1208792f284c0a6ee6696a228ed5b8abf349def3
Raul Astete <raulastete.2215@gmail.com>
2024-11-28 10:23:47 -0500

    Implement Browse Screen Navigation

---
+++ app/src/main/java/com/raulastete/todoistclone/browse/BrowseNavigation.kt
@@ -0,0 +1,27 @@
+package com.raulastete.todoistclone.browse
+
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.raulastete.todoistclone.navigation.Route
+
+fun NavGraphBuilder.BrowseNavigation(
+    navController: NavHostController
+) {
+
+    composable<Route.Main.Browse> {
+        BrowseScreen(
+            browseViewModel = viewModel(),
+            onNavigateToProductivity = { navController.navigate(Route.Main.Productivity) },
+            onNavigateToNotifications = { navController.navigate(Route.Main.Notifications) },
+            onNavigateToSettings = { navController.navigate(Route.Configuration.Settings) },
+            onNavigateToInbox = { navController.navigate(Route.Main.Inbox) },
+            onNavigateToActivityLog = { navController.navigate(Route.Main.ActivityLog) },
+            onNavigateToFiltersNTags = { navController.navigate(Route.Main.FiltersNTags) },
+            onNavigateToCreateProject = { navController.navigate(Route.Main.CreateProject) },
+            onNavigateToProject = { projectId -> navController.navigate(Route.Main.Project(projectId)) },
+            onNavigateToManageProjects = { navController.navigate(Route.Main.ManageProjects) }
+        )
+    }
+}
