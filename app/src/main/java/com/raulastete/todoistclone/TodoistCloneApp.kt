6faa638f535462731f56dad1c69c85cbcfc5feb2
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2024-12-02 18:15:45 -0500

    Refactor DI module location

--- app/src/main/java/com/raulastete/todoistclone/TodoistCloneApp.kt
+++ app/src/main/java/com/raulastete/todoistclone/TodoistCloneApp.kt
@@ -4,9 +4,9 @@ import android.app.Application
 import com.raulastete.todoistclone.presentation.features.browse.browseModule
 import com.raulastete.todoistclone.di.appModule
 import com.raulastete.todoistclone.di.repositoryModule
-import com.raulastete.todoistclone.projects.creation.projectCreationModule
-import com.raulastete.todoistclone.projects.detail.projectModule
-import com.raulastete.todoistclone.projects.management.projectManagementModule
+import com.raulastete.todoistclone.presentation.features.projects.creation.projectCreationModule
+import com.raulastete.todoistclone.presentation.features.projects.detail.projectModule
+import com.raulastete.todoistclone.presentation.features.projects.management.projectManagementModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
@@ -19,8 +19,12 @@ class TodoistCloneApp : Application() {
             androidLogger()
             androidContext(this@TodoistCloneApp)
             modules(
-                appModule + repositoryModule + projectCreationModule + projectModule + browseModule
-                + projectManagementModule
+                appModule,
+                repositoryModule,
+                projectCreationModule,
+                projectModule,
+                browseModule,
+                projectManagementModule
             )
         }
     }
