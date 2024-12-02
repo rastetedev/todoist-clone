13dd02b88ecb389c523a835deec30a93183c3f09
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2024-12-02 18:02:09 -0500

    Rename navigation bar config destination logic file

---
+++ app/src/main/java/com/raulastete/todoistclone/presentation/features/settings/navigationbar/NavigationBarConfigDestination.kt
@@ -0,0 +1,19 @@
+package com.raulastete.todoistclone.presentation.features.settings.navigationbar
+
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.raulastete.todoistclone.presentation.navigation.Route
+
+fun NavGraphBuilder.navigationBarConfigDestination(
+    navController: NavHostController
+) {
+
+    composable<Route.NavigationBarConfig> {
+        NavigationBarConfigScreen(
+            navigationBarConfigViewModel = viewModel(),
+            onNavigateBack = { navController.popBackStack() },
+        )
+    }
+}
